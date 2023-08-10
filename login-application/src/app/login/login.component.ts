d4178829f2e5f2cc59ab7ae7d36d2a5906fa997a
Veljko Bubnjevic <veljkobubnjevic01@gmail.com>
2023-08-10 09:13:50 +0200

    initialize form group

--- login-application/src/app/login/login.component.ts
+++ login-application/src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import {User} from "../models/User";
+import {FormControl, FormGroup, Validator, Validators} from "@angular/forms";
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -11,10 +12,21 @@ export class LoginComponent {
 
   constructor(private router: Router) { }
 
+  isValid : boolean = true;
+  loginForm  = new FormGroup({
+    email : new FormControl('', [Validators.required, Validators.email]),
+    password : new FormControl('', [Validators.required])
+  });
+
   onRegister(){
     this.router.navigate(['/register']);
   }
   onLogin() {
+    if (!this.loginForm.valid) {
+      this.isValid = false;
+      return;
+    }
+
     this.router.navigate(['/login']);
   }
 
