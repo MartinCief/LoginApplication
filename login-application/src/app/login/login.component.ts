281941c35d6937923b6dfef5fac90cec85c3b2fe
Veljko Bubnjevic <veljkobubnjevic01@gmail.com>
2023-08-10 08:19:36 +0200

    added login form control

--- login-application/src/app/login/login.component.ts
+++ login-application/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+
+  constructor() {
+  }
+  loginForm = new FormGroup({
+    username : new FormControl('', [Validators.required]),
+    password : new FormControl('', [Validators.required])
+  })
+
+ login() {
+
+    if (!this.loginForm.valid) {
+      return;
+    }
+    const loginInfo = {
+      username : this.loginForm.value.username,
+      password : this.loginForm.value.password
+    }
+
+ }
 
 }
