34933388dd94ea5de16457dfeddfc5a9cc6006f1
Veljko Bubnjevic <veljkobubnjevic01@gmail.com>
2023-08-10 09:54:37 +0200

    added form builder

--- login-application/src/app/login/login.component.ts
+++ login-application/src/app/login/login.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import {User} from "../models/User";
-import {FormControl, FormGroup, Validator, Validators} from "@angular/forms";
+import {FormBuilder, FormControl, FormGroup, Validator, Validators} from "@angular/forms";
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -10,22 +10,20 @@ import {FormControl, FormGroup, Validator, Validators} from "@angular/forms";
 export class LoginComponent {
 
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private formBuilder : FormBuilder) { }
 
   isValid : boolean = true;
-  loginForm  = new FormGroup({
-    email : new FormControl('', [Validators.required, Validators.email]),
-    password : new FormControl('', [Validators.required])
-  });
+
+
+  loginForm = this.formBuilder.group({
+    email : ['', Validators.required, Validators.email],
+    password : ['', Validators.required]
+  })
 
   onRegister(){
     this.router.navigate(['/register']);
   }
   onLogin() {
-    if (!this.loginForm.valid) {
-      this.isValid = false;
-      return;
-    }
 
     this.router.navigate(['/login']);
   }
